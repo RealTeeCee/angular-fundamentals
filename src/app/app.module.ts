1da8094259f53367f0092e5c49bfe7e423b4e4a4
Nguyen Ngoc Nguyen <nguyenngocnguyen.rtc@gmail.com>
2023-10-13 11:20:31 +0700

    ok

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,17 +4,17 @@ import { EventsAppComponent } from './events-app.component';
 import { EventsListComponent } from './events/events-list.component';
 import { EventsThumbnailComponent } from './events/events-thumbnail.component';
 import { NavbarComponent } from './nav/navbar.component';
-
+import { EventsService } from './events/shared/event.service';
+import { ToastrService } from './common/toastr.service';
 @NgModule({
   imports: [BrowserModule],
-  declarations: 
-  [
+  declarations: [
     EventsAppComponent,
     EventsListComponent,
     EventsThumbnailComponent,
     NavbarComponent,
   ],
-
+  providers: [EventsService, ToastrService],
   bootstrap: [EventsAppComponent],
 })
 export class AppModule {}
